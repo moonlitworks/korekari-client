d923cb955ae8affb1f82b1657c548cd6b4b37ca6
eyzi <contact@eyzi.dev>
2022-05-24 07:56:11 -0400

    chore(pkg): vue app boilerplate

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <KoreKari />
+</template>
+
+<script>
+import KoreKari from "./components/KoreKari.vue";
+
+export default {
+  name: "App",
+  components: {
+    KoreKari,
+  },
+};
+</script>
