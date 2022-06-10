3753352f6f4f7a5444c5087f115d1b3687195f64
eyzi <contact@eyzi.dev>
2022-06-10 01:18:53 -0400

    build(pwa): remove potential infinite update loop

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,9 +19,8 @@ if (process.env.NODE_ENV === 'production') {
     updatefound() {
       console.log('New content is downloading.')
     },
-    updated(registration) {
+    updated() {
       console.log('New content is available; please refresh.')
-      registration.update()
       window.location.reload(true)
     },
     offline() {
